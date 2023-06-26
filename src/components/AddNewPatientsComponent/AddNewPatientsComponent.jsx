7a2c937e009b15d89116eba88cb47e28ecf3be92
navyavenkat <navyavenkat03@gmail.com>
2023-06-25 20:59:49 -0700

    Components are used in react

---
+++ src/components/AddNewPatientsComponent/AddNewPatientsComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddNewPatientsComponent = () => {
+  return (
+    <div>
+        <h1>AddNewPatientsComponent</h1></div>
+  )
+}
+
+export default AddNewPatientsComponent
+
